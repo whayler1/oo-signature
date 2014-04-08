04190a7cf3405b7522932a9ce72c65d80ddbfdd8
Justin Worsdale <whayler1@gmail.com>
2014-04-08 17:49:52 -0400

    wrote in some header comments

--- oo-signature.js
+++ oo-signature.js
@@ -1,3 +1,18 @@
+/**
+ * This module returns the signature that is required for an Ooyala API request.
+ *
+ * @param {String} apiKey The api key from your backlot account.
+ * @param {String} secretKey The secret key from your backlot account.
+ * @param {String|Integer} The expires arg for your api request. You can generate
+ * 	one easily with this equation: Math.round(new Date().getTime() / 1000) + 120
+ * @param {path} path The api request path, sans any query string.
+ * @param {String|undefined} method The request method. Defaults to 'GET' if undefined.
+ *
+ * @return {String} signatureStr The signature.
+ *
+ * @author Justin Worsdale
+ */
+
 var crypto = require('crypto');
 
 module.exports = function(apiKey, secretKey, expires, path, method) {
